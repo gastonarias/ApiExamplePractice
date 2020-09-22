3b9af1470cf1080f7fe761e2a99af0703dc68faa
Gaston Ariel Arias <gastonarielarias@gmail.com>
2020-09-22 11:19:19 -0300

    log

---
+++ PrimerWebApi/Helpers/Log/Logger.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrimerWebApi.Helpers.Log
+{
+    public class Logger : ILogger
+    {
+        public IDisposable BeginScope<TState>(TState state)
+        {
+            return null;
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            return true;
+        }
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+
+            var msg = formatter(state, exception);
+
+            var json = JsonConvert.SerializeObject(new
+            {
+                logLevel = logLevel,
+                eventId = eventId,
+                logDateTimeUtc = DateTime.UtcNow,
+                details = msg,
+                exception = exception
+            });
+
+            File.AppendAllText(@"C:\temp\log.txt", json);
+        }
+    }
+}
---
+++ PrimerWebApi/Helpers/Log/LoggerProvider.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using PrimerWebApi.Helpers.Log;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrimerWebApi.Helpers
+{
+    public class LoggerProvider : ILoggerProvider
+    {
+        private readonly IConfiguration configuration;
+        private ILogger logger;
+        private bool disposed = false;
+
+        public LoggerProvider()
+        {
+
+        }
+
+        public LoggerProvider(IConfiguration config )
+        {
+            configuration = config;
+        }
+        public ILogger CreateLogger(string categoryName)
+        {
+            if (logger == null)
+                return new Logger();
+
+            return logger;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    logger = null;
+                }
+
+                disposed = true;
+            }
+        }
+    }
+}
